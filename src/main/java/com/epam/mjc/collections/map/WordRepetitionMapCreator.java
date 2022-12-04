93fedef253b52bcc69af3e2f734e7a136811f89d
onisim_albarov <onisim93@gmail.com>
2022-12-04 21:13:10 +0400

    complete stage1-module4-collections-task3

--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,22 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> result = new HashMap<>();
+        String[] words = sentence.replaceAll("[^\\w|\\s]", "").split(" ");
+        if (words.length > 1) {
+            for (String word : words) {
+                String lowerCaseWord = word.toLowerCase(Locale.ROOT);
+                if (result.putIfAbsent(lowerCaseWord, 1) != null) {
+                    result.computeIfPresent(lowerCaseWord, (key, value) -> value + 1);
+                }
+            }
+        }
+
+        return result;
     }
 }
